c3d285d1e4d90c6e398b95c6fe6395bd9e49530a
Zaeem96 <zaeem1996@hotmail.com>
2018-11-14 13:21:33 +0100

    changed state in login to TS-state

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -3,25 +3,31 @@ import '../stylesheets/loginStyle.css';
 import { Link } from 'react-router-dom'
 import Store from '../store/Store'
 
-class Login extends React.Component<any, any> {
+interface userInfo {
+    username: string | undefined,
+    password: string | undefined
+}
 
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            username: "",
-            password: ""
-        }
+class Login extends React.Component<any, userInfo> {
+
+    state = {
+        username: undefined,
+        password: undefined
     }
 
-    // made for test purposes since no endpoint is being served
     login() {
-        Store.token = "asdb22222";
+        //@ts-ignore
+        Store.loginUser(this.state.username, this.state.password)
         this.props.history.push("/")
     }
 
-    handleChange = (e: any) => {
-        this.setState({ [e.target.name]: e.target.value });
-    };
+    handleUserName(event: React.FormEvent<HTMLInputElement>) {
+        this.setState({ username: event.currentTarget.value })
+    }
+
+    handlePassword(event: React.FormEvent<HTMLInputElement>) {
+        this.setState({ password: event.currentTarget.value })
+    }
 
     public render() {
 
@@ -36,15 +42,13 @@ class Login extends React.Component<any, any> {
                         <p className="message">Already registered? <a href="#">Sign In</a></p>
                     </div>
                     <div className="login-form">
-                        <input type="text" name="username" placeholder="username" onChange={this.handleChange} />
-                        <input type="password" name="password" placeholder="password" onChange={this.handleChange} />
+                        <input type="text" name="username" placeholder="username" onChange={(e) => this.handleUserName(e)} />
+                        <input type="password" name="password" placeholder="password" onChange={(e) => this.handlePassword(e)} />
                         <button onClick={() => {
-                            Store.loginUser(this.state.username, this.state.password)
-                            this.props.history.push("/")
+                            this.login()
                         }}>Login</button>
                         <p className="message textContainer"> Not Registered? <Link to="/register">Create an account!</Link></p>
                         <p className="message textMargin">Forgot Password? <Link to="/resetPassword">Reset Password</Link></p>
-
                     </div>
                 </div>
             </div>
